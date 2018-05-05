844851eb3cd0d33ffba117f70a36bc7728e223e8
CaseyInHaengsin <casey.rowley89@gmail.com>
2018-05-04 21:36:33 -0600

    adding to github.

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,5 +2,16 @@ const utils = require('./utils');
 
 it('Should add two numbers', () => {
     var res = utils.add(33, 11);
-    throw new Error('Value not correct');
+
+    if (res !== 44){
+        throw new Error(`Expected 44, but got ${res}`);
+    }
+});
+
+it('Should square a number', () => {
+      var res = utils.square(3);
+
+      if (res !== 9){
+          throw new Error(`Expecting 9 and got ${res}`);
+    }
 });
