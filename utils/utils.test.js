87bd2f50ca3341c7cd09081e85f7f2ca0f21f13d
CaseyInHaengsin <casey.rowley89@gmail.com>
2018-05-05 20:21:47 -0600

    added testing for objects

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,54 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('Should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if (res !== 44){
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+
+    expect(res).toBe(44).toBeA('number');
+
 });
+
+    //expect(res).toBe(44);
 
 it('Should square a number', () => {
       var res = utils.square(3);
 
-      if (res !== 9){
-          throw new Error(`Expecting 9 and got ${res}`);
-    }
+      expect(res).toBe(9).toBeA('number');
+
+
 });
+
+    //should verify that first and last names are set
+
+    it('should verify first and last names are set', () => {
+        //assert it includes first name and last name with proper values
+        var user = {
+            location: 'Provo',
+            age: 25
+        };
+        var res = utils.setName(user, 'Josh Ericson');
+
+        //expect(user).toEqual(res);
+        expect(res).toInclude({
+            firstName: 'Casey',
+            lastName: 'Rowley'
+        })
+
+
+    });
+
+
+
+/*
+it('should expect some values', () => {
+   //expect(12).toNotBe(13);
+    expect({
+        name: 'Casey',
+        age: 25,
+        location: 'Philadelphia'
+    }).toInclude({
+        age: 23
+    })
+});
+*/
